5f2a25350b78bca3b71625e9ce32d6de0af65770
Rishika <2480082@cognizant.com>
2026-03-25 17:19:58 +0530

    Added controller and model classes for sprint and project

--- project_sphere/src/main/java/com/cts/mfrp/project_sphere/model/Project.java
+++ project_sphere/src/main/java/com/cts/mfrp/project_sphere/model/Project.java
@@ -1,4 +1,39 @@
 package com.cts.mfrp.project_sphere.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "project")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Project {
+
+    @Id
+    @Column(name = "project_id", length = 50)
+    private String projectId;
+
+    @Column(name = "project_name", length = 255, nullable = false)
+    private String projectName;
+
+    @Column(name = "project_manager_id", length = 50)
+    private String projectManagerId;
+
+    @OneToMany(mappedBy = "project")
+    @Builder.Default
+    private List<Sprint> sprints = new ArrayList<>();
 }
+

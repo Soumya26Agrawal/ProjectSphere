9382af4921efb28fd8875b24ffa0c38abafb81e4
Kandagatla <2480175@cognizant.com>
2026-03-26 21:30:49 +0530

    created tickethistory and user endpoints

--- project_sphere/src/main/java/com/cts/mfrp/project_sphere/service/TicketHistoryService.java
+++ project_sphere/src/main/java/com/cts/mfrp/project_sphere/service/TicketHistoryService.java
@@ -1,4 +1,20 @@
 package com.cts.mfrp.project_sphere.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cts.mfrp.project_sphere.model.TicketHistory;
+import com.cts.mfrp.project_sphere.repository.TicketHistoryRepository;
+
+@Service
 public class TicketHistoryService {
+
+    @Autowired
+    TicketHistoryRepository ticketHistoryRepository;
+
+    public List<TicketHistory> findTicketHistory(long ticketId) {
+        return ticketHistoryRepository.findByTicketIdOrderByTimeStampDesc(ticketId);
+    }
 }

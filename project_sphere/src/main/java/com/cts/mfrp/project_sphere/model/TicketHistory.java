e9a6c9bee7487f5fc8a1d3a8619a6d38f5dbe0e7
Kandagatla <2480175@cognizant.com>
2026-03-26 18:19:18 +0530

    updated user and ticketHistory models

--- project_sphere/src/main/java/com/cts/mfrp/project_sphere/model/TicketHistory.java
+++ project_sphere/src/main/java/com/cts/mfrp/project_sphere/model/TicketHistory.java
@@ -1,4 +1,56 @@
 package com.cts.mfrp.project_sphere.model;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "ticket_history")
 public class TicketHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private long historyId;
+
+    @ManyToOne(fetch = FetchType.LAZY)  //one ticket can have many ticket histories
+    @JoinColumn(name = "ticket_id", nullable = false)
+    private Ticket ticket;
+
+    @Column(name = "changed_by", nullable = false)
+    private long changedBy;   //referencing to user_id
+
+    @Column(name = "field_changed", nullable = false)
+    private String fieldChanged;  //status, assigne, any ticket field 
+
+    @Column(name = "old_value", nullable = false)
+    private String oldValue;
+
+    @Column(name = "new_value", nullable = false)
+    private String newValue;
+
+    @CreationTimestamp
+    @Column(name = "changed_at", updatable = false)
+    private LocalDateTime timeStamp;
+
 }
--- project_sphere/src/main/java/com/cts/mfrp/project_sphere/repository/UserRepository.java
+++ project_sphere/src/main/java/com/cts/mfrp/project_sphere/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.cts.mfrp.project_sphere.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cts.mfrp.project_sphere.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmployeeId(Long employeeId);
 }
